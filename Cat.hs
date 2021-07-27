fd2a61113154f831d9dc8399d4183b93b3cb7f04
Bernardo Gomes Negri <38192358+BernardoGomesNegri@users.noreply.github.com>
2021-07-27 12:21:08 -0300

    added files

---
+++ Cat.hs
@@ -0,0 +1,54 @@
+module Main where
+import System.Environment
+import System.Exit
+import Data.Char
+
+main :: IO ()
+main = getArgs >>= parse >>= putStr
+
+bfilterString :: Int -> [String] -> [String]
+bfilterString int [] = []
+bfilterString int strings =
+    if head strings /= "" then
+        (show int ++ " " ++ head strings) : bfilterString (int + 1) (tail strings)
+    else
+        bfilterString (int + 1) (tail strings)
+
+nfilterString :: Int -> [String] -> [String]
+nfilterString int [] = []
+nfilterString int strings = (show int ++ " " ++ head strings) : nfilterString (int + 1) (tail strings)
+
+sfilterString :: [String] -> [String]
+sfilterString [] = []
+sfilterString strings =
+    if head strings == "" && strings !! 1 == "" then
+        --Eliminar esta linha
+        sfilterString (tail strings)
+    else
+        head strings : sfilterString (tail strings)
+
+vfilterString :: [String] -> [String]
+vfilterString strings = filter (not . isPrint) (head strings) : vfilterString (tail strings)
+
+readFileArg :: [String] -> ([String] -> [String]) -> IO String
+readFileArg fs fn = fmap (unlines . fn . lines . concat) (mapM readFile (fs))
+
+parse :: [String] -> IO String
+parse ["--help"] = usage   >> exit
+parse ["--version"] = version >> exit
+parse []     = getContents
+parse fs
+    | head fs == "-b" =
+        readFileArg (tail fs) (bfilterString 1)
+    | head fs == "-n" =
+        readFileArg (tail fs) (nfilterString 1)
+    | head fs == "-s" =
+        readFileArg (tail fs) sfilterString
+    | head fs == "-v" =
+        readFileArg (tail fs) vfilterString
+    | otherwise =
+        concat `fmap` mapM readFile fs
+
+usage   = putStrLn "Usage: cat [-vh] [file ..]"
+version = putStrLn "Haskell cat 0.1"
+exit    = exitSuccess
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HaskellCat
 An implementation of the Unix "cat" command in Haskell, the basic taken from https://wiki.haskell.org/Tutorials/Programming_Haskell/Argument_handling, but changed a little.
+To build, simply run "ghc Cat" on this directory
